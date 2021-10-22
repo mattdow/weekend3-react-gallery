6eaae059e7e07627fc790216569d607f018ae67c
Matt Dow <matt.dow.vo@gmail.com>
2021-10-22 15:15:55 -0500

    Added GET request to App component, tested

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  // Define and initialize our array of images, and the method we use to set their state.
+  let [galleryItems, setGalleryItems] = useState([]);
+
+  // define a function to GET the gallery items from the route /gallery
+  const getGalleryItems = () => {
+    axios
+      .get('/gallery')
+      .then((response) => {
+        console.log('Gallery list is ->', response.data);
+        // Use the set state function to load the array of photos from the server response
+        setGalleryItems(response.data);
+        // Prepare a console.log in case of error in the GET request.
+      }).catch((err) => {
+        console.log('Error getting gallery', err);
+      })
+
+  }
+
+  // call useEffect to intialize the page and grab the initial array of photos from the server
+  useEffect(() => {
+    getGalleryItems();
+  }, []);
+  // render the content from App to the DOM
     return (
       <div className="App">
         <header className="App-header">
