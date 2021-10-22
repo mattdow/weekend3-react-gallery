2ac843fc599cd446e2b17243655668f2bdadee0d
Matt Dow <matt.dow.vo@gmail.com>
2021-10-22 16:08:27 -0500

    Added two photos and tested GalleryItem

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Ira_mini.jpg', description: 'The extraterrestrial Ira, who took the form of a ridiculous dog.', likes: 1},
+    { id: 3, path: 'images/mattdow_mini.jpg', description: 'A Matt Dow appears in his natural environment.', likes: 0}
 ];
 
 module.exports = galleryItems;
